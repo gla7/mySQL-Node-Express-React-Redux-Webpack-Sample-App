221bbf59156e502ed9bbcadf2e6509e8d2f254c4
Gabriel Leon <gabriel.leonaveleyra@tapinfluence.com>
2016-06-23 19:01:59 -0600

    have gotten half of the charts to work still need others to work too

--- components/Analytics.js
+++ components/Analytics.js
@@ -15,6 +15,33 @@ class Analytics extends Component {
 
 	render() {
 
+		var polarData = {
+		    datasets: [{
+		        data: [
+		            11,
+		            16,
+		            7,
+		            3,
+		            14
+		        ],
+		        backgroundColor: [
+		            "#FF6384",
+		            "#4BC0C0",
+		            "#FFCE56",
+		            "#E7E9ED",
+		            "#36A2EB"
+		        ],
+		        label: 'My dataset' // for legend
+		    }],
+		    labels: [
+		        "Red",
+		        "Green",
+		        "Yellow",
+		        "Grey",
+		        "Blue"
+		    ]
+		};
+
 		var dataDoughnut = {
 		    labels: [
 		        "Red",
@@ -64,15 +91,92 @@ class Analytics extends Component {
 		    ]
 		};
 
+		var barData = {
+		    labels: ["January", "February", "March", "April", "May", "June", "July"],
+		    datasets: [
+		        {
+		            label: "My First dataset",
+		            backgroundColor: "rgba(255,99,132,0.2)",
+		            borderColor: "rgba(255,99,132,1)",
+		            borderWidth: 1,
+		            hoverBackgroundColor: "rgba(255,99,132,0.4)",
+		            hoverBorderColor: "rgba(255,99,132,1)",
+		            data: [65, 59, 80, 81, 56, 55, 40],
+		        }
+		    ]
+		};
+
+		var radarData = {
+		    labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
+		    datasets: [
+		        {
+		            label: "My First dataset",
+		            backgroundColor: "rgba(179,181,198,0.2)",
+		            borderColor: "rgba(179,181,198,1)",
+		            pointBackgroundColor: "rgba(179,181,198,1)",
+		            pointBorderColor: "#fff",
+		            pointHoverBackgroundColor: "#fff",
+		            pointHoverBorderColor: "rgba(179,181,198,1)",
+		            data: [65, 59, 90, 81, 56, 55, 40]
+		        },
+		        {
+		            label: "My Second dataset",
+		            backgroundColor: "rgba(255,99,132,0.2)",
+		            borderColor: "rgba(255,99,132,1)",
+		            pointBackgroundColor: "rgba(255,99,132,1)",
+		            pointBorderColor: "#fff",
+		            pointHoverBackgroundColor: "#fff",
+		            pointHoverBorderColor: "rgba(255,99,132,1)",
+		            data: [28, 48, 40, 19, 96, 27, 100]
+		        }
+		    ]
+		};
+
+		var pieData = {
+		    labels: [
+		        "Red",
+		        "Blue",
+		        "Yellow"
+		    ],
+		    datasets: [
+		        {
+		            data: [300, 50, 100],
+		            backgroundColor: [
+		                "#FF6384",
+		                "#36A2EB",
+		                "#FFCE56"
+		            ],
+		            hoverBackgroundColor: [
+		                "#FF6384",
+		                "#36A2EB",
+		                "#FFCE56"
+		            ]
+		        }]
+		};
+
 		let Linechart = ReactChart.Line
+
+		let Barchart = ReactChart.Bar
+
+		let Radarchart = ReactChart.Radar
+
+		let Polarareachart = ReactChart.PolarArea
+
+		let Piechart = ReactChart.Pie
 
 		let Doughnutchart = ReactChart.Doughnut
 
-		console.log(Linechart)
+		
+
+		
 
 		return  <div className='fiftyPxIndentation'>
 					<h1>Analytics is under construction- examples of graphics can be found below...</h1>
 					<Linechart data={dataLine} width="600" height="250" redraw/>
+					<Barchart data={barData}  width="600" height="250" redraw/>
+					<Radarchart data={radarData} width="600" height="250" redraw/>
+					<Polarareachart data={polarData} width="600" height="250" redraw/>
+					<Piechart data={pieData} width="600" height="250" redraw/>
 					<Doughnutchart data={dataDoughnut} width="600" height="250" redraw/>
 				</div>
 				
